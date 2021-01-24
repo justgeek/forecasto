e88456105cfa2c1ef4dfcffaecc45f0aea453328
Mohamed Taher <mt@prollygeek.com>
2021-01-24 17:08:44 +0100

    - implement empty state

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,12 +2,12 @@ import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CityeatherCardComponent } from './components/city-weather-card/city-weather-card.component';
+import { EmptyHomeComponent } from './components/empty-home/empty-home.component';
 import { HomePage } from './home.page';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [HomePage,CityeatherCardComponent],
-  providers:[],
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+  declarations: [HomePage, CityeatherCardComponent, EmptyHomeComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageModule {}
